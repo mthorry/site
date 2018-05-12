b33fb12acbc770f4fe8e63dbfeaa9ab29ef7830a
mthorry <mthorry@gmail.com>
2018-05-12 08:29:31 -0400

    blog images

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import listeners from '../images/listeners.png'
+import site from '../images/site.png'
 import redux from '../images/redux.png'
 import google from '../images/google.png'
 import performance from '../images/performance.png'
@@ -8,29 +8,43 @@ class Blog extends Component {
   render() {
     return (
       <div className="Blog section flex-container">
-      <div className="item">
-          <img src={google} className="thumb"/>
-          <div className="blog-overlay">
-            <div className="text">
-              <h3>Google Maps and React 🗺</h3>
-              <p><strong>Featured in Frontend Weekly</strong></p>
-              <p>Currently over 8000 views</p>
-              <p><a href="https://medium.com/front-end-hacking/using-the-google-maps-javascript-api-in-a-react-project-b3ed734375c6" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
-            </div>
-          </div>
-        </div>
 
         <div className="item">
           <img src={redux} className="thumb"/>
           <div className="blog-overlay">
             <div className="text">
-              <h3>My First Redux App 🥇</h3>
-              <p><strong>Featured in Hacker Noon</strong></p>
-              <p>Currently over 2000 views</p>
-              <p><a href="https://hackernoon.com/my-first-redux-app-with-a-rails-backend-4cf59332a994" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
+              <h3>Why I Love Redux 💜</h3>
+              <p><strong>Featured in Frontend Weekly</strong></p>
+              <p>Currently over 3000 views</p>
+              <p><a href="https://medium.com/front-end-hacking/why-i-love-redux-with-react-and-so-should-you-def71cee6a26" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
             </div>
           </div>
         </div>
+
+      <div className="item">
+          <img src={google} className="thumb"/>
+          <div className="blog-overlay">
+            <div className="text">
+              <h3>Google Maps and React 🗺</h3>
+              <p><strong>Featured in Hackernoon</strong></p>
+              <p>Currently over 5000 views</p>
+              <p><a href="https://hackernoon.com/how-to-make-a-personal-site-with-react-and-javascript-%EF%B8%8F-6fdf26211124" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src={site} className="thumb"/>
+          <div className="blog-overlay">
+            <div className="text">
+              <h3>How I Made This Site 🔨</h3>
+              <p><strong>Featured in Frontend Weekly</strong></p>
+              <p>Currently over 5000 views</p>
+              <p><a href="https://medium.com/front-end-hacking/using-javascript-event-listeners-and-why-javascript-isnt-all-bad-bcd6cdf7f1f9" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
+            </div>
+          </div>
+        </div>
+
         <div className="item">
           <img src={performance} className="thumb"/>
           <div className="blog-overlay">
@@ -39,17 +53,6 @@ class Blog extends Component {
               <p><strong>Featured in Hacker Noon</strong></p>
               <p>Currently over 800 views</p>
               <p><a href="https://medium.com/@mthorry/application-performance-monitoring-with-newrelic-chrome-9910a379fb4" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
-            </div>
-          </div>
-        </div>
-        <div className="item">
-          <img src={listeners} className="thumb"/>
-          <div className="blog-overlay">
-            <div className="text">
-              <h3>JavaScript Event Listeners👂</h3>
-              <p><strong>Featured in Frontend Weekly</strong></p>
-              <p>Currently over 500 views</p>
-              <p><a href="https://medium.com/front-end-hacking/using-javascript-event-listeners-and-why-javascript-isnt-all-bad-bcd6cdf7f1f9" target="_blank" rel="noopener noreferrer">Read on Medium  <i className="fa fa-external-link"></i></a></p>
             </div>
           </div>
         </div>
