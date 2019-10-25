e1178700f500aa9ff4ebc99d20561530b8dac6c8
mthorry <mthorry@gmail.com>
2019-10-25 12:27:57 -0400

    delete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import Welcome from './components/Welcome'
-import Projects from './components/Projects'
-import Teaching from './components/Teaching'
-import About from './components/About'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import arrow from './images/arrow.gif'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          {/* <NavBar /> */}
           <Welcome />
-          {/* <div className="down-arrow">
-          <img src={arrow} alt="Sroll"  width="50px"/>
-          </div>
-        <h2 className="heading" id="projects">PROJECTS</h2>
-          <Projects />
-        <h2 className="heading" id="about">ABOUT</h2>
-          <About />
-        <h2 className="heading" id="teaching">TEACHING</h2>
-          <Teaching />
-        <h2 className="heading" id="footer">LIKE WHAT YOU SEE?</h2>
-          <Footer /> */}
       </div>
     );
   }
